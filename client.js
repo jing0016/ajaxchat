d51c16b590107ff96fbf01c1825a66c6e2b36af3
jing0016 <32021491+jing0016@users.noreply.github.com>
2018-07-13 15:11:06 -0400

    commit

--- client.js
+++ client.js
@@ -11,9 +11,46 @@ var pollServer = function () {
             var chatBubble;
 
             if(this.sent_by == 'self'){
-                chatBubble = $('<div></div>')
+                chatBubble = $('<div class="row bubble-sent pull-right"> this.message </div><div class="clearfix"></div>');
+            }
+            else{
+                chatBubble = $('<div class="row bubble-recv"> this.message </div><div class="clearfix"></div>');
             }
 
-        })
+            $('#chatPanel').append(chatBubble);
+
+        });
+
+        setTimeout(pollServer, 5000);
     });
 }
+
+$(document).on('ready',function(){
+    pollServer();
+
+    $('button').click(function () {
+        $(this).toggleClass('active');
+    });
+});
+
+$('#sendMessageBtn').on('click',function (event) {
+    event.preventDefault();
+
+    var message = $('#chatMessage').val();
+
+    $.post('chat.php',{
+        'message' : message
+    }, function (result) {
+
+        $('#sendMessageBtn').toggleClass('active');
+
+        if(!result.success){
+            alert("There was an error sending your message");
+        }
+        else{
+            console.log("Message sent!");
+            $('#chatMessage').val('');
+        }
+
+    });
+});
